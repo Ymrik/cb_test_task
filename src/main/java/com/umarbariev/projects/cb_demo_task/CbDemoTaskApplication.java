9976b96e1db3240219600925ca902ca4cf15328d
Umar Bariev <ubariev@mail.ru>
2022-09-06 20:18:04 +0700

    init commit

---
+++ src/main/java/com/umarbariev/projects/cb_demo_task/CbDemoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.umarbariev.projects.cb_demo_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbDemoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbDemoTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umarbariev/projects/cb_demo_task/CbDemoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umarbariev.projects.cb_demo_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbDemoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
